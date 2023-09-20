0f2da5bf89ab42a25773506e5b646b9b43095423
arn0 <arnoc@xs4all.nl>
2023-09-20 15:03:57 +0200

    add esp_transport_tcp

--- main/main.c
+++ main/main.c
@@ -9,6 +9,9 @@
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
+#include "esp_transport.h"
+#include "esp_transport_tcp.h"
+
 
 #include "../../secret.h"
 
@@ -170,6 +173,58 @@ void wifi_init_sta(void)
     }
 }
 
+static const char *payload = "Message from ESP32\n";
+
+
+static void tcp_transport_client_task(void *pvParameters)
+{
+    static const char *TAG = ">>> tcp_transport_client_task";
+    char rx_buffer[128];
+    char host_ip[] = SECRET_IP;
+    esp_transport_handle_t transport = esp_transport_tcp_init();
+
+    while (1) {
+        if (transport == NULL) {
+            ESP_LOGE(TAG, "Error occurred during esp_transport_proxy_init()");
+            break;
+        }
+        int err = esp_transport_connect(transport, SECRET_IP, SECRET_PORT, -1);
+        if (err != 0) {
+            ESP_LOGE(TAG, "Client unable to connect: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Successfully connected");
+
+        while (1) {
+            int bytes_written = esp_transport_write(transport, payload, strlen(payload), 0);
+            if (bytes_written < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: esp_transport_write() returned %d, errno %d", bytes_written, errno);
+                break;
+            }
+            int len = esp_transport_read(transport, rx_buffer, sizeof(rx_buffer) - 1, 0);
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recv failed: esp_transport_read() returned %d, errno %d", len, errno);
+                break;
+            }
+            // Data received
+            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+            ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+            ESP_LOGI(TAG, "Received data : %s", rx_buffer);
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+
+        ESP_LOGE(TAG, "Shutting down transport and restarting...");
+        esp_transport_close(transport);
+    }
+    esp_transport_destroy(transport);
+
+  vTaskDelete(NULL);
+}
+
+
+
 void app_main(void)
 {
     //Initialize NVS
@@ -183,4 +238,7 @@ void app_main(void)
     wifi_init_sta();
 
     vTaskDelay( 5000 / portTICK_PERIOD_MS );
+
+    xTaskCreate(tcp_transport_client_task, "tcp_transport_client", 4096, NULL, 5, NULL);
+
 }
